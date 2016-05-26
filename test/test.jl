b4fec7874a9351a803af717cd311c2044ea33c56
Pierre-Yves Gerardy <py@py.gy>
2016-05-27 01:44:02 +0200

    Add tests for line endings

--- test/test.jl
+++ test/test.jl
@@ -43,6 +43,47 @@ end
 # valid = filter(valid) do n ;; match(r"table-array", n) == nothing end
 
 function test()
+
+    # Inline test for line endings
+    # (since git can mess it up in files we use strings)
+    print("Valid crlf: ")
+    try
+        tml = TOML.parse("foo = 5\r\nbar = 6")
+        jsn = jsn2data(JSON.parse("{ \"foo\": {\"type\":\"integer\", \"value\":\"5\"}, \"bar\":{\"type\":\"integer\", \"value\":\"6\"} }"))
+        if jsn == tml
+            display_success("Ok")
+        else
+            display_error("unexpected result.\nTOML:\n$tml\n\nJSON:\n$jsn\n")
+            global exitstatus = 1
+        end
+    catch err
+        if !isa(err, TOML.TOMLError)
+            rethrow(err)
+        end
+        display_error("couldn't be parsed.\n" * repr(err))
+        global exitstatus = 1
+    end
+
+    print("Valid lf: ")
+    try
+        tml = TOML.parse("foo = 5\nbar = 6")
+        jsn = jsn2data(JSON.parse("{ \"foo\": {\"type\":\"integer\", \"value\":\"5\"}, \"bar\":{\"type\":\"integer\", \"value\":\"6\"} }"))
+        if jsn == tml
+            display_success("Ok")
+        else
+            display_error("unexpected result.\nTOML:\n$tml\n\nJSON:\n$jsn\n")
+            global exitstatus = 1
+        end
+    catch err
+        if !isa(err, TOML.TOMLError)
+            rethrow(err)
+        end
+        display_error("couldn't be parsed.\n" * repr(err))
+        global exitstatus = 1
+    end
+
+
+
     for t in valid
         print("Valid $t: ")
             open(string(testdir, "/valid/", t, ".toml")) do tml
@@ -66,6 +107,58 @@ function test()
             end
         end
     end
+
+    println("Count \\n end of lines:")
+    try
+        tml = TOML.parse("\ninvalid")
+        display_error("should have failed but didn't.")
+        print(tml)
+        global exitstatus = 1
+    catch err
+        if !isa(err, TOML.TOMLError)
+            rethrow(err)
+        end
+        if err.msg == "Unexpected end of file on line 2."
+            display_success("  " * repr(err))
+        else
+            display_error("  " * repr(err) * "\nexpected\n  " * "Unexpected end of file on line 2.")
+        end
+    end
+
+    println("Count \\r end of lines:")
+    try
+        tml = TOML.parse("\rinvalid")
+        display_error("should have failed but didn't.")
+        print(tml)
+        global exitstatus = 1
+    catch err
+        if !isa(err, TOML.TOMLError)
+            rethrow(err)
+        end
+        if err.msg == "Unexpected end of file on line 2."
+            display_success("  " * repr(err))
+        else
+            display_error(repr(err) * "\n  expected\n" * "TOMLError: Unexpected end of file on line 2.")
+        end
+    end
+
+    println("Count \\r\\n end of lines:")
+    try
+        tml = TOML.parse("\r\ninvalid")
+        display_error("should have failed but didn't.")
+        print(tml)
+        global exitstatus = 1
+    catch err
+        if !isa(err, TOML.TOMLError)
+            rethrow(err)
+        end
+        if err.msg == "Unexpected end of file on line 2."
+            display_success("  " * repr(err))
+        else
+            display_error("  " * repr(err) * "\nexpected\n  " * "Unexpected end of file on line 2.")
+        end
+    end
+
 
     for t in invalid
         println("Invalid $t:")
